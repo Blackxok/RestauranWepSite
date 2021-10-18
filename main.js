6afe429c808bf59029c4bcf4119a2a45e21b70ad
Blackxok <nasridoni1@gmail.com>
2021-10-18 09:46:03 +0500

    This is my RestauranWebSite

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+// ----------------- Nav toggler---------------
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav() {
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open")
+}
+
+
+// ----------close nav---------
+document.addEventListener("click", function (e) {
+    if (e.target.closest(".nav-item")) {
+        toggleNav();
+    }
+})
+
+// ----------------stickiy header -----------
+window.addEventListener("scroll", function () {
+    if (this.pageYOffset > 60) {
+        document.querySelector(".header").classList.add("stckiy")
+    }
+    else {
+        document.querySelector(".header").classList.remove("stckiy")
+    }
+})
+
+// ------------------------menu tabs --------------------------
+
+const menuTabs = document.querySelector(".menu-tabs");
+
+menuTabs.addEventListener("click", function (e) {
+    if (e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")) {
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+});
